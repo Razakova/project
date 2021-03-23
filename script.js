918ddef7633f6175c19ec4bb1ed5982b78d44bda
Aruzhan <razak_arujan@mail.ru>
2021-03-23 23:04:20 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr);
+console.log(decr);
+
+
+
+
+
+
+
+
+// const category = 'toys';
+
+// console.log(`http://someurl.com/${category}/5`);
+
+// console.log("Hello world");
+
+
+
+// const user = "Ivan";
+
+// alert(`Privet, ${user}`);
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+
+// const obj = {
+//     a: 50
+// };
+
+
+// obj.a = 10;
+// console.log(obj);
+
+
